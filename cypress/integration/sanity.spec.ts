6b44aa5b471e4e397e514a21681d692e52112109
Murat Ozcan <muratkerem@gmail.com>
2021-09-21 13:18:10 -0500

    baseline

---
+++ cypress/integration/sanity.spec.ts
@@ -0,0 +1,6 @@
+describe('app-a', () => {
+  it('passes sanity', () => {
+    cy.visit('/');
+    cy.contains('Hello world');
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello world.
         </p>
         <a
           className="App-link"
