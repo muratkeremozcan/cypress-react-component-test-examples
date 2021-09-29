9a7b67c3b7a66670d506ad379fc6690cf950e4b3
Murat Ozcan <muratkerem@gmail.com>
2021-09-29 13:44:56 -0500

    test cypress.env.json

--- cypress/integration/sanity.spec.ts
+++ cypress/integration/sanity.spec.ts
@@ -2,5 +2,9 @@ describe('app-a', () => {
   it('passes sanity', () => {
     cy.visit('/');
     cy.contains('Hello world');
+    cy.log('my secret is ', Cypress.env('SECRET'));
+    console.log('my secret is ', Cypress.env('SECRET'));
+
+    cy.wrap(Cypress.env('SECRET')).should('equal', 'at app abc');
   })
 })
