f92b245493d997e340373faf92fce7a0d44498ac
Murat Ozcan <muratkerem@gmail.com>
2021-11-15 08:50:43 -0600

    separate yml for component vs e2e

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { mount } from '@cypress/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-it('renders learn react link', () => {
-  mount(<App />);
-  cy.get('a').contains('Learn React');
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
