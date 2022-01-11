563dc5c0ecc2b9843575c2e1ce7378c0353d2622
Murat Ozcan <muratkerem@gmail.com>
2022-01-11 09:39:56 -0600

    finished styled components, downgraded react scripts to v4

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -23,3 +23,11 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('getByCy', (selector, ...args) =>
+  cy.get(`[data-cy="${selector}"]`, ...args)
+)
+
+Cypress.Commands.add('getByCyLike', (selector, ...args) =>
+  cy.get(`[data-cy*=${selector}]`, ...args)
+)
