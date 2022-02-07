d50371ba1d370a301e2d3d56d523070cdc2b82c6
Murat Ozcan <muratkerem@gmail.com>
2022-02-07 11:31:36 -0600

    added examples from basarat

---
+++ cypress/component/basarat-codes/conditional-rendering/1.falsies.comp-spec.tsx
@@ -0,0 +1,24 @@
+import { mount } from '@cypress/react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div>false: {false}</div>
+      <div>null: {null}</div>
+      <div>undefined: {undefined}</div>
+      <div>'': {''}</div>
+      <div>
+        <p>React renders numeric falsy values</p>
+        <div data-cy="0">0: {0}</div>
+        <div data-cy="NaN">NaN: {NaN}</div>
+      </div>
+    </div>
+  )
+}
+
+it('should render numeric falsy values', () => {
+  mount(<App />)
+  cy.getByCy('0').should('contain', '0')
+  cy.getByCy('NaN').should('contain', 'NaN')
+})
---
+++ cypress/component/basarat-codes/conditional-rendering/2.basic-conditional-rendering.comp-spec.tsx
@@ -0,0 +1,88 @@
+import { useState } from 'react'
+import './App.css'
+import { mount } from '@cypress/react'
+
+it('should operate basic counter', () => {
+  function App() {
+    const [count, setCount] = useState(0)
+    const inc = () => setCount((clicks) => clicks + 1)
+    const dec = () => setCount((clicks) => clicks - 1)
+
+    return (
+      <div className="App">
+        <div>
+          <button data-cy="inc" onClick={inc}>
+            +
+          </button>
+          <button data-cy="dec" onClick={dec}>
+            -
+          </button>
+          <p data-cy="count">Count: {count}</p>
+        </div>
+      </div>
+    )
+  }
+
+  mount(<App />)
+  cy.getByCy('count').should('contain', 'Count: 0')
+  cy.getByCy('inc').dblclick()
+  cy.getByCy('count').should('contain', 'Count: 2')
+
+  cy.getByCy('dec').dblclick().dblclick()
+  cy.getByCy('count').should('contain', 'Count: -2')
+})
+
+it('should operate basic toggler', () => {
+  function App() {
+    const [isShown, setIsShown] = useState(false)
+    const toggle = () => setIsShown((shown) => !shown)
+
+    return (
+      <div className="App">
+        <button onClick={toggle} data-cy="toggle">
+          Toggle
+        </button>
+        {/* prefer && over ternary */}
+        {isShown && <p data-cy="content">Conditional Content</p>}
+        {isShown ? <p data-cy="content">Conditional Content</p> : null}
+      </div>
+    )
+  }
+
+  mount(<App />)
+  cy.getByCy('content').should('not.exist')
+  cy.getByCy('toggle').click()
+  cy.getByCy('content').should('be.visible')
+  cy.getByCy('toggle').click()
+  cy.getByCy('content').should('not.exist')
+})
+
+it('should load & clear user', () => {
+  type User = { name: string } | null
+  function App() {
+    const [user, setUser] = useState<User>(null)
+    const load = () => setUser({ name: 'Jane' })
+    const clear = () => setUser(null)
+
+    return (
+      <div className="App">
+        <button onClick={load} data-cy="load">
+          Load
+        </button>
+        <button onClick={clear} data-cy="clear">
+          Clear
+        </button>
+        {/* prefer && over ternary */}
+        {user && <p data-cy="user">{user.name}</p>}
+        {user ? <p data-cy="user">{user.name}</p> : null}
+      </div>
+    )
+  }
+
+  mount(<App />)
+  cy.getByCy('user').should('not.exist')
+  cy.getByCy('load').click()
+  cy.getByCy('user').should('contain', 'Jane')
+  cy.getByCy('clear').click()
+  cy.getByCy('user').should('not.exist')
+})
