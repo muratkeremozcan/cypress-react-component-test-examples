98c64b52a13f0b6d481c8352efbf2346f9e769f5
Murat Ozcan <muratkerem@gmail.com>
2022-03-14 11:09:07 -0500

    fixed type issues

--- cypress/component/hooks/react-infinite-scroll-hook/InfiniteListWithReverseVerticalScroll.cy.tsx
+++ cypress/component/hooks/react-infinite-scroll-hook/InfiniteListWithReverseVerticalScroll.cy.tsx
@@ -3,34 +3,30 @@ import InfiniteListWithReverseVerticalScroll from './InfiniteListWithReverseVert
 import { recurse } from 'cypress-recurse'
 
 // @ts-ignore
-describe(
-  'InfiniteListWithReverseVerticalScroll',
-  { viewportWidth: 600 },
-  () => {
-    it('should render items lower down the infinite list', () => {
-      mount(<InfiniteListWithReverseVerticalScroll />)
-      cy.getByCyLike('list-item-10').should('be.visible')
+describe('ReverseVerticalScroll', { viewportWidth: 600 }, () => {
+  it('should render items lower down the infinite list', () => {
+    mount(<InfiniteListWithReverseVerticalScroll />)
+    cy.getByCyLike('list-item-10').should('be.visible')
 
-      const itemId = '30'
+    const itemId = '30'
 
-      recurse(
-        () =>
-          cy
-            .getByCy(`list-item-${itemId}`, { log: false })
-            .should(Cypress._.noop),
-        ($item) => $item.text().includes(itemId),
-        {
-          log: false,
-          limit: 10,
-          timeout: 10000,
-          delay: 1000,
-          post() {
-            cy.getByCy('list-container').scrollTo('top', { log: false })
-          }
+    recurse(
+      () =>
+        cy
+          .getByCy(`list-item-${itemId}`, { log: false })
+          .should(Cypress._.noop),
+      ($item) => $item.text().includes(itemId),
+      {
+        log: false,
+        limit: 10,
+        timeout: 10000,
+        delay: 1000,
+        post() {
+          cy.getByCy('list-container').scrollTo('top', { log: false })
         }
-      )
-        .scrollIntoView()
-        .invoke('css', 'border', '2px solid red')
-    })
-  }
-)
+      }
+    )
+      .scrollIntoView()
+      .invoke('css', 'border', '2px solid red')
+  })
+})
