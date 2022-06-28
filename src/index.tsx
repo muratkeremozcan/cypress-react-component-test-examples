f1498c5326f1e119bd07e12df2a182f114f01654
Murat Ozcan <muratkerem@gmail.com>
2022-06-28 17:36:57 +0300

    migrated to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+// @ts-expect-error
+ReactDOM.createRoot(document.getElementById('root')).render(
+   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ 
