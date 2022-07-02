c64a6894f2523cec1e5b3f9470fde7d3ce53ac9b
Murat Ozcan <muratkerem@gmail.com>
2022-07-02 18:04:46 +0300

    useContext, useCallback, useMemo, useReducer

---
+++ cypress/component/hooks/kyle-wds/useMemo/useMemo.cy.jsx
@@ -0,0 +1,17 @@
+import App from './useMemo'
+
+describe('useMemo', () => {
+  it('should memoize slowFunction and call it again upon changing theme', () => {
+    cy.window()
+      .its('console')
+      .then((console) => cy.spy(console, 'log').as('log'))
+    cy.mount(<App />)
+
+    cy.get('@log').invoke('resetHistory')
+    cy.getByCy('number').type('2')
+    cy.get('@log').should('be.calledOnceWith', 'calling slow function')
+
+    cy.getByCy('toggle-theme').click()
+    cy.get('@log').should('be.calledOnce')
+  })
+})
