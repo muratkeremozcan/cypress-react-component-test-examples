41d6791894f0e4346aba31ef21a3a99142cab43a
Murat Ozcan <muratkerem@gmail.com>
2022-07-12 15:27:23 +0300

    finished

--- cypress/component/hooks/kyle-wds/custom-hooks/30-useLongPress.js/useLongPress.cy.js
+++ cypress/component/hooks/kyle-wds/custom-hooks/30-useLongPress.js/useLongPress.cy.js
@@ -6,6 +6,9 @@ it('useLongPress', () => {
 
   cy.getByCy('element').should('have.css', 'background-color', 'rgb(255, 0, 0)')
 
+  cy.getByCy('element').click()
+  cy.get('@alert').should('not.be.called')
+
   cy.getByCy('element').realMouseDown()
   cy.get('@alert').should('be.calledOnceWith', 'Long Press')
 })
