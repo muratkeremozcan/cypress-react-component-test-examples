5b2c6e9cf08ecf2cad07350edbb845ffbe807456
Murat Ozcan <muratkerem@gmail.com>
2022-08-23 11:24:40 -0500

    refactored modal example

--- cypress/component/hooks/kent.c.dodds-epic-react/06.1.testingJs/modal-portals.ts/Modal.tsx
+++ cypress/component/hooks/kent.c.dodds-epic-react/06.1.testingJs/modal-portals.ts/Modal.tsx
@@ -1,27 +1,26 @@
 import { useEffect, useRef, ReactNode } from 'react'
 import { createPortal } from 'react-dom'
 
-let modalRoot = document.getElementById('modal-root') as HTMLElement
-if (!modalRoot) {
-  modalRoot = document.createElement('div')
-  modalRoot.setAttribute('id', 'modal-root')
-  document.body.appendChild(modalRoot)
-}
-
-interface ModalProps {
+type ModalProps = {
   children?: ReactNode
 }
 
 const Modal = ({ children }: ModalProps) => {
   const el = useRef(document.createElement('div'))
 
+  let modalRoot = document.getElementById('modal-root')
+  if (!modalRoot) {
+    modalRoot = document.createElement('div')
+    modalRoot.setAttribute('id', 'modal-root')
+    document.body.appendChild(modalRoot)
+  }
+
   useEffect(() => {
-    // Use this in case CRA throws an error about react-hooks/exhaustive-deps
     const currentEl = el.current
 
     modalRoot.appendChild(currentEl)
-    return () => void modalRoot!.removeChild(currentEl)
-  }, [])
+    return () => void modalRoot.removeChild(currentEl)
+  }, [modalRoot])
 
   return createPortal(children, el.current)
 }
