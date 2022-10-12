5067d8446b3665628bc007b3ccacfc872cc7adf2
Murat Ozcan <muratkerem@gmail.com>
2022-10-12 11:17:59 -0500

    6 hook mistakes test toggle

--- cypress/component/hooks/kyle-wds/6-hooks-mistakes/1.using-state-when-not-needed/useRef-not-useState-formSubmit-pipe.cy.tsx
+++ cypress/component/hooks/kyle-wds/6-hooks-mistakes/1.using-state-when-not-needed/useRef-not-useState-formSubmit-pipe.cy.tsx
@@ -6,21 +6,21 @@ import { pipe } from 'ramda'
 function App() {
   // mistake: the state variables are only used in onSubmit;
   // we don't care what state they are in until submitted, but we re-render on every change
-  // const [email, setEmail] = useState('')
+  // const [email, setEmail] = useState('') // (toggle)
   // const [password, setPassword] = useState('')
 
-  // fix: use useRef instead of useState when you don't care about the form input state until the form is submitted
+  // fix: use useRef instead of useState when you don't care about the form input state until the form is submitted (toggle)
   const emailRef = useRef<HTMLInputElement>(null)
   const passwordRef = useRef<HTMLInputElement>(null)
 
-  // handleSubmit classic version
+  // handleSubmit classic version // (toggle 2)
   // const handleSubmit = (e: FormEvent) => {
   //   e.preventDefault()
-  //   // console.log({ email, password })
-  //   console.log({ email: emailRef.current.value, password: passwordRef.current.value })
+  //   // console.log({ email, password }) // toggle
+  //   console.log({ email: emailRef.current.value, password: passwordRef.current.value }) // toggle
   // }
 
-  // handleSubmit FP version
+  // handleSubmit FP version // (toggle 2)
   const preventDefault = (e) => {
     e.preventDefault()
     return e
@@ -31,22 +31,26 @@ function App() {
   })
   const handleSubmit = pipe(preventDefault, destructEvent, console.log)
 
+  console.log('render')
+
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="email">Email</label>
       <input
         id="email"
         type="email"
-        ref={emailRef}
-        /*value={email} onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}*/
+        // value={email} // (toggle)
+        // onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} // (toggle)
+        ref={emailRef} // (toggle)
       />
 
       <label htmlFor="password">Password</label>
       <input
         id="password"
         type="password"
-        ref={passwordRef}
-        /*value={password} onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}*/
+        // value={password} // (toggle)
+        // onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} // (toggle)
+        ref={passwordRef} // (toggle)
       />
       <button data-cy="submit" type="submit">
         Submit
@@ -64,7 +68,8 @@ it('use useRef instead of useState when you do not care about the form input sta
   const password = '123456'
   cy.get('#email').type(email)
   cy.get('#password').type(password)
-  cy.getByCy('submit').click()
+  cy.get('@consoleLog').should('be.calledWith', 'render').its('callCount').should('eq', 1)
 
-  cy.get('@consoleLog').should('be.calledWith', { email, password })
+  cy.getByCy('submit').click()
+  cy.get('@consoleLog').its('callCount').should('eq', 2)
 })
