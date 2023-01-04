1ad533972fa712afd9c3d98332d0fa7df4d61b2a
Murat Ozcan <muratkerem@gmail.com>
2023-01-04 07:21:16 -0600

    useParams with custom route

--- cypress/component/hooks/kyle-wds/react-router-advanced/react-router-advanced.cy.jsx
+++ cypress/component/hooks/kyle-wds/react-router-advanced/react-router-advanced.cy.jsx
@@ -2,52 +2,74 @@ import App from './app'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 describe('react router', () => {
-  beforeEach(() => {
-    cy.mount(
-      <Router>
-        <>
-          <App />
-        </>
-      </Router>
-    )
+  context('click on links to get a route', () => {
+    beforeEach(() => {
+      cy.mount(
+        <Router>
+          <>
+            <App />
+          </>
+        </Router>
+      )
+    })
+    it('should nav to Home, not found', () => {
+      cy.contains('Books').click().location('pathname').should('equal', '/books')
+
+      cy.contains('Home').click().location('pathname').should('equal', '/')
+
+      cy.contains('Not found')
+        .should('have.css', 'color', 'rgb(0, 0, 0)')
+        .click()
+        .should('have.css', 'color', 'rgb(255, 0, 0)')
+        .location('pathname')
+        .should('equal', '/not-found')
+    })
+
+    it(' should nav to books, book 1, 2 and new ', () => {
+      cy.contains('Books').click().location('pathname').should('equal', '/books')
+      cy.getByCy('sidebar').should('be.visible')
+
+      cy.contains('Book 1').click().location('pathname').should('equal', '/books/1')
+      cy.getByCy('sidebar').should('not.exist')
+
+      cy.contains('Book 2').click().location('pathname').should('equal', '/books/2')
+      cy.getByCy('sidebar').should('not.exist')
+
+      cy.contains('New Book').click().location('pathname').should('equal', '/books/new')
+      cy.getByCy('sidebar').should('not.exist')
+
+      cy.contains('Books').click()
+      cy.getByCy('sidebar').should('be.visible')
+    })
   })
 
-  it('should nav to Home, not found', () => {
-    cy.contains('Books').click().location('pathname').should('equal', '/books')
+  context('trick the CT runner into having a url', () => {
+    it('should navigate to /books/new', () => {
+      window.history.pushState({}, null, '/books/new')
+      cy.mount(
+        <Router>
+          <>
+            <App />
+          </>
+        </Router>
+      )
 
-    cy.contains('Home').click().location('pathname').should('equal', '/')
+      cy.location('pathname').should('equal', '/books/new')
+      cy.getByCy('sidebar').should('not.exist')
+    })
 
-    cy.contains('Not found')
-      .should('have.css', 'color', 'rgb(0, 0, 0)')
-      .click()
-      .should('have.css', 'color', 'rgb(255, 0, 0)')
-      .location('pathname')
-      .should('equal', '/not-found')
-  })
+    it('should navigate to /books', () => {
+      window.history.pushState({}, null, '/books')
+      cy.mount(
+        <Router>
+          <>
+            <App />
+          </>
+        </Router>
+      )
 
-  it(' should nav to books, book 1, 2 and new ', () => {
-    cy.contains('Books').click().location('pathname').should('equal', '/books')
-    cy.getByCy('sidebar').should('be.visible')
-
-    cy.contains('Book 1')
-      .click()
-      .location('pathname')
-      .should('equal', '/books/1')
-    cy.getByCy('sidebar').should('not.exist')
-
-    cy.contains('Book 2')
-      .click()
-      .location('pathname')
-      .should('equal', '/books/2')
-    cy.getByCy('sidebar').should('not.exist')
-
-    cy.contains('New Book')
-      .click()
-      .location('pathname')
-      .should('equal', '/books/new')
-    cy.getByCy('sidebar').should('not.exist')
-
-    cy.contains('Books').click()
-    cy.getByCy('sidebar').should('be.visible')
+      cy.location('pathname').should('equal', '/books')
+      cy.getByCy('sidebar').should('be.visible')
+    })
   })
 })
