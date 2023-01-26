e0ac4ddb796da436f9c681e0d50dde902998761b
Murat Ozcan <muratkerem@gmail.com>
2023-01-26 17:43:35 -0600

    barebones

--- cypress/component/okta-login/okta.tsx
+++ cypress/component/okta-login/okta.tsx
@@ -1,22 +1,23 @@
-const redirectUrl = async (): Promise<string> => {
-  const state = String(Cypress._.random(16))
-  localStorage.setItem('state', state)
+// const redirectUrl = async (): Promise<string> => {
+//   const state = String(Cypress._.random(16))
+//   localStorage.setItem('state', state)
 
-  const url = 'www.google.com'
+//   const url = 'www.google.com'
 
-  return url
-}
+//   return url
+// }
 
 export const OktaLoginForm = () => {
   const handleSubmit = async (): Promise<void> => {
-    const oktaRedirect = await redirectUrl()
-    window.location.assign(oktaRedirect)
+    window.location.assign('www.google.com')
   }
 
   return (
     <form>
-      <div onSubmit={handleSubmit}>
-        <button data-cy="login-button">Log in</button>
+      <div>
+        <button onClick={handleSubmit} data-cy="login-button">
+          Log in
+        </button>
       </div>
     </form>
   )
