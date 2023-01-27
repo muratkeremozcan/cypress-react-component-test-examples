8e195b8b14826ef0b8b04aede1cad9a941eda3c6
Murat Ozcan <muratkerem@gmail.com>
2023-01-27 10:14:47 -0600

    make window.location.replace work in a component test

--- cypress/component/redirect/window.location.replace/login.cy.tsx
+++ cypress/component/redirect/window.location.replace/login.cy.tsx
@@ -1,26 +1,26 @@
 import { LoginBtn } from './login'
 
-describe('should deal with win.location.replace', () => {
-  it('render login form and click to redirect', () => {
-    // window.location is locked down for security reasons.
-    // During the test, we can use the cy.intercept command to modify the application code.
-    //  For example, it could call window.__location.assign instead.
-    //  Our test would create this dummy window.__location object before the component mounts
-    cy.on('window:before:load', (win) => {
-      // @ts-expect-error this is a nice hack
-      win.__location = {
-        assign: cy.stub().as('assign')
-      }
-    })
+it('should deal with win.location.replace', () => {
+  // how would we do the same workaround in a component test?
+  // cypress/e2e/win-location-replace.spec.ts
 
-    cy.intercept('GET', '*', (req) => {
-      req.continue((res) => {
-        res.body = res.body.replaceAll('window.location.assign', 'window.__location.assign')
-      })
-    }).as('stubAssign')
-    Cypress.on('uncaught:exception', () => false)
-    cy.mount(<LoginBtn />)
-
-    // cy.getByCy('login-button').click()
+  cy.on('window:before:load', (win) => {
+    // @ts-ignore
+    win.__location = {
+      assign: cy.stub().as('assign')
+    }
   })
+
+  cy.intercept('GET', 'https://www.cypress.io', (req) => {
+    req.continue((res) => {
+      res.body = res.body.replaceAll('window.location.assign', 'window.__location.assign')
+    })
+  }).as('index')
+
+  cy.mount(<LoginBtn />)
+
+  cy.getByCy('login-button')
+  // .click()
+  // cy.wait('@index')
+  // cy.get('@assign').should('have.been.calledOnceWith', 'https://www.cypress.io')
 })
