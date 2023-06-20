4c08efffef5e33cc00dc4d9a7b53fbabba942c43
Murat Ozcan <muratkerem@gmail.com>
2023-06-20 13:10:35 -0500

    added comment

--- cypress/component/css-modules/Button.tsx
+++ cypress/component/css-modules/Button.tsx
@@ -5,6 +5,20 @@ type ButtonProps = {
   label: string
 }
 
-export default function Button({ type, label }: ButtonProps) {
-  return <button className={styles[type]}>{label}</button>
+export default function Button({ type, label }: ButtonProps): JSX.Element {
+  return (
+    <button type="button" className={styles[type]}>
+      {label}
+    </button>
+  )
 }
+
+/*
+if there is trouble with TS importing .module.css, create a file `css-modules.d.ts` with the following content:
+
+declare module '*.module.css' {
+  const classes: { [key: string]: string }
+  export default classes
+}
+
+*/
