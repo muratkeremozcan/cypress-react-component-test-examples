b278c722829f88502dc1860cae9539d9b0bf960e
Murat Ozcan <muratkerem@gmail.com>
2023-09-05 10:58:55 -0500

    recreate config file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,26 +1,20 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   // TODO: there is 1 test in CI that fails with this on, when not parallelized
   // cypress/component/hooks/usehooks.com/useHover.cy.tsx
   // experimentalSingleTabRunMode: true, //
-  projectId: '62pyqm',
+  // projectId: '62pyqm',
   defaultCommandTimeout: 10000,
   retries: {
     runMode: 2,
     openMode: 0
   },
-  e2e: {
-    setupNodeEvents(on, config) {},
-    // baseUrl: 'http://localhost:3000', // need to test index.html in another test...
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}'
-  },
   component: {
-    setupNodeEvents(on, config) {},
     devServer: {
-      framework: 'create-react-app',
-      bundler: 'webpack'
+      framework: "create-react-app",
+      bundler: "webpack",
     },
     specPattern: ['**/**/*.comp-spec.{js,ts,jsx,tsx}', '**/**/*.cy.{js,ts,jsx,tsx}']
-  }
-})
+  },
+});
