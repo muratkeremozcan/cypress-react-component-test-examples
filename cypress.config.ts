2bbe5b1e83b89c4a82c1cb75c2864de1ee67f273
Murat Ozcan <muratkerem@gmail.com>
2023-09-07 14:27:38 -0500

    projectId enabled

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
   // TODO: there is 1 test in CI that fails with this on, when not parallelized
   // cypress/component/hooks/usehooks.com/useHover.cy.tsx
   // experimentalSingleTabRunMode: true, //
-  // projectId: '62pyqm',
+  projectId: '62pyqm',
   defaultCommandTimeout: 10000,
   retries: {
     runMode: 2,
@@ -18,9 +18,9 @@ export default defineConfig({
   component: {
     setupNodeEvents(on, config) {},
     devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
+      framework: 'create-react-app',
+      bundler: 'webpack'
     },
     specPattern: ['**/**/*.comp-spec.{js,ts,jsx,tsx}', '**/**/*.cy.{js,ts,jsx,tsx}']
-  },
-});
+  }
+})
